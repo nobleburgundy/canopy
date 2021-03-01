3a5abfc9636b6098270dcbfd73443bdf4f79b801
James Gould <nobleburgundy@gmail.com>
2021-02-28 20:40:13 -0600

    Implemented react-bootstrap-table-next component

--- src/pages/EmployeePage/index.js
+++ src/pages/EmployeePage/index.js
@@ -3,9 +3,36 @@ import EmployeeTable from "../../components/EmployeeTable";
 import FilterRow from "../../components/FilterRow";
 import Header from "../../components/Header";
 import API from "../../utils/api";
+import BootstrapTable, { TableHeaderColumn } from "react-bootstrap-table-next";
 
 function EmployeePage() {
   const [employees, setEmployees] = useState([]);
+  const columns = [
+    {
+      dataField: "id",
+      text: "ID",
+    },
+    {
+      dataField: "first_name",
+      text: "First Name",
+      sort: true,
+    },
+    {
+      dataField: "last_name",
+      text: "Last Name",
+      sort: true,
+    },
+    {
+      dataField: "title",
+      text: "Role",
+      sort: true,
+    },
+    {
+      dataField: "state",
+      text: "State",
+      sort: true,
+    },
+  ];
 
   useEffect(() => {
     console.log("loading employees");
@@ -23,8 +50,20 @@ function EmployeePage() {
   return (
     <div className="container">
       <Header />
-      <FilterRow />
-      <EmployeeTable employees={employees} />
+      {/* <FilterRow /> */}
+      {/* <EmployeeTable employees={employees} /> */}
+      <div className="table-responsive">
+        <BootstrapTable
+          keyField="id"
+          columns={columns}
+          data={employees}
+          bootstrap4
+          hover
+          condensed
+          bordered
+          classes="table text-monospace"
+        ></BootstrapTable>
+      </div>
     </div>
   );
 }
