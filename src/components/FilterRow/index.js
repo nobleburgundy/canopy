51f07722ea66654d3bb3f7158837eb0d4b091038
James Gould <nobleburgundy@gmail.com>
2021-02-28 20:39:29 -0600

    Added FilterRow component

--- src/components/FilterRow/index.js
+++ src/components/FilterRow/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function FilterRow() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <div className="form-row">
+          <div className="col">
+            <input type="text" className="form-control" placeholder="First name" />
+          </div>
+          <div className="col">
+            <input type="text" className="form-control" placeholder="Last name" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default FilterRow;
