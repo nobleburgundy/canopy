88b50a0511ca2ba5edb64bbb994baea723f7760b
James Gould <james.gould@kincentric.com>
2021-03-01 11:43:09 -0600

    Added some basic UI tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Expected UI elements", () => {
+  test("renders Welcome to Canopy heading", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Welcome to Canopy/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders employee table", () => {
+    render(<App />);
+    const table = screen.getByRole("table");
+    expect(table).toBeInTheDocument();
+  });
+
+  test("renders search bar", () => {
+    render(<App />);
+    const searchBar = screen.getByRole("textbox");
+    expect(searchBar).toBeInTheDocument();
+  });
+
+  test("renders Role filter", () => {
+    render(<App />);
+    const input = screen.getByLabelText("Filter by Role:");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("renders State filter", () => {
+    render(<App />);
+    const input = screen.getByLabelText("Filter by State:");
+    expect(input).toBeInTheDocument();
+  });
 });
