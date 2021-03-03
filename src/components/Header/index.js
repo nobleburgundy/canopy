d145e6b14546243a96c93d0b9a0b9f42b4b48285
James Gould <nobleburgundy@gmail.com>
2021-03-02 18:39:23 -0600

    Added link to main page from jumbotron

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,10 @@ import "./style.css";
 
 function Header() {
   return (
-    <div className="jumbotron">
-      <h1>Welcome to Canopy</h1>
+    <div className="jumbotron mb-1">
+      <h1>
+        <a href="/">Welcome to Canopy</a>
+      </h1>
     </div>
   );
 }
